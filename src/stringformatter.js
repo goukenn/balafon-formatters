b098766982c8e56d0e8afe2b33c6850b5d49a637
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2024-03-14 19:01:48 +0100

    constant passing to parent buffer...error

--- src/stringformatter.js
+++ src/stringformatter.js
@@ -151,7 +151,8 @@ let lines = [
     // "if ( true )",
      // "{ { { return }" // missing 2
      // "{ { { return " // missing 3
-     "{{if (true){ return }}}" // missing 1 // error
+     "{{if (true){ return 8",
+     "}}}" // missing 1 // error
     //"tour d'ivoire"
 ];
 _formatter.listener = null;
