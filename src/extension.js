f5df44877ee9afa585ce398218e4a81a411eb11a
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2024-04-11 14:58:57 +0200

    add logo

---
+++ src/extension.js
@@ -0,0 +1,19 @@
+const vscode = require('vscode');
+
+/**
+ * 
+ * @param {*} context 
+ */
+function activate(context){
+
+}
+/**
+ * 
+ */
+function deactivate(){
+
+}
+
+// export extension method
+exports.activate = activate;
+exports.deactivate = deactivate;
